06b02a2702eff7612d840a23178097e30a87c26f
hHanzou <erickvictor1404@gmail.com>
2025-05-22 14:38:42 -0300

    add base url for deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/sabaoladora/'
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   vite: {
